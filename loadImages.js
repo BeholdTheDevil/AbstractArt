10f095d917f71692046ee5a8837f4430461ee503
BeholdTheDevil <antdeg@outlook.com>
2017-01-26 09:54:51 +0100

    Temporary botchwork.
    Now has static filenames instead of dynamic ones

--- loadImages.js
+++ loadImages.js
@@ -1,4 +1,4 @@
-var dir = "/AbsractArt/images/";
+/*var dir = "/AbstractArt/images/";
 var fileextension = [".jpg", ".png", ".bmp", ".gif", ".tif"];
 var fileList = [];
 
@@ -7,8 +7,8 @@ $.ajax({url: dir,
 			var index = 0;
 			$(fileextension).each(function() {
 				$(data).find("a:contains(" + fileextension[index] + ")").each(function() {
-					if(fileList.indexOf("/AbstractArt" + this.href.replace(window.location.host, "").replace("https://", "")) == -1) {
-						fileList.push("/AbstractArt" + this.href.replace(window.location.host, "").replace("https://", ""));
+					if(fileList.indexOf(this.href.replace(window.location.host, "").replace("https://", "")) == -1) {
+						fileList.push(this.href.replace(window.location.host, "").replace("https://", ""));
 					}
 				});
 				index = index + 1;
@@ -33,4 +33,25 @@ $.ajax({url: dir,
     	        document.getElementById("flexBox").appendChild(div);
 			}
 		}
-});
+});*/
+
+var fileList = ["/images/a_red_11.jpg", "/images/a_red_3.jpg", "/images/e_cyan_4.jpg", "/images/g_purple_1.jpg", "/images/h_pink_7.png", "/images/a_red_12.jpg", "/images/b_orange_2.jpg", "/images/f_blue_10.jpg", "/images/g_purple_5.png", "/images/a_red_13.jpg", "/images/d_green_14.jpg", "/images/f_blue_9.jpg", "/images/h_pink_6.jpg"];
+fileList.sort();
+
+for(var i = 0; i < fileList.length; i++) {
+	var filename = fileList[i]
+    var div = document.createElement("div");
+    var rndNum = Math.floor((Math.random() * 6) + 1);
+    if(rndNum == 1 || rndNum == 2 || rndNum == 3) {
+    	div.setAttribute("class", "sizeOne tile");
+	} else if(rndNum == 4 || rndNum == 5) {
+    	div.setAttribute("class", "sizeTwo tile");
+    } else if(rndNum == 6) {
+    	div.setAttribute("class", "sizeThree tile");
+    }
+    div.setAttribute("id", "image" + (i * 33));
+    div.style.backgroundImage = 'url(' + dir + filename + ')';
+   	div.content = "&nbsp";
+   	div.onclick = function() {model(this.id)};
+	document.getElementById("flexBox").appendChild(div);
+}
