1838c126f9d2e935497dbd61dd5a6283e073bee8
BeholdTheDevil <antdeg@outlook.com>
2017-01-26 09:00:35 +0100

    Hotfix-path 2

--- loadImages.js
+++ loadImages.js
@@ -1,4 +1,4 @@
-var dir = "/AbstractArt/images";
+var dir = "/images";
 var fileextension = [".jpg", ".png", ".bmp", ".gif", ".tif"];
 var fileList = [];
 
@@ -7,8 +7,8 @@ $.ajax({url: dir,
 			var index = 0;
 			$(fileextension).each(function() {
 				$(data).find("a:contains(" + fileextension[index] + ")").each(function() {
-					if(fileList.indexOf(this.href.replace(window.location.host, "").replace("http://", "")) == -1) {
-						fileList.push(this.href.replace(window.location.host, "").replace("http://", ""));
+					if(fileList.indexOf("/AbstractArt" + this.href.replace(window.location.host, "").replace("http://", "")) == -1) {
+						fileList.push("/AbstractArt" + this.href.replace(window.location.host, "").replace("http://", ""));
 					}
 				});
 				index = index + 1;
