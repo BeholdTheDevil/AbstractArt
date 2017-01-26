2e29e81f330550a527204ea1e216ed297562a56e
BeholdTheDevil <antdeg@outlook.com>
2017-01-26 08:36:18 +0100

    Addition of a sort of model that appears when the user clicks an image

--- loadImages.js
+++ loadImages.js
@@ -16,11 +16,9 @@ $.ajax({url: dir,
 			fileList.sort();
 			
 			for(var i = 0; i < fileList.length; i++) {
-				console.log(fileList[i]);
 				var filename = fileList[i]
             	var div = document.createElement("div");
             	var rndNum = Math.floor((Math.random() * 6) + 1);
-            	console.log(rndNum);
             	if(rndNum == 1 || rndNum == 2 || rndNum == 3) {
             		div.setAttribute("class", "sizeOne tile");
             	} else if(rndNum == 4 || rndNum == 5) {
@@ -28,8 +26,10 @@ $.ajax({url: dir,
             	} else if(rndNum == 6) {
             		div.setAttribute("class", "sizeThree tile");
             	}
+            	div.setAttribute("id", "image" + (i * 33));
             	div.style.backgroundImage = 'url(' + dir + filename + ')';
    	   		    div.content = "&nbsp";
+   	   		    div.onclick = function() {model(this.id)};
     	        document.getElementById("flexBox").appendChild(div);
 			}
 		}
