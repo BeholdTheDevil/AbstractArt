a100cda84399c8d0ee765e6f31bb5c1f46019405
BeholdTheDevil <antdeg@outlook.com>
2017-02-09 08:51:53 +0100

    Upload for testing

--- loadImages.js
+++ loadImages.js
@@ -1,57 +1,23 @@
-/*var dir = "/AbstractArt/images/";
-var fileextension = [".jpg", ".png", ".bmp", ".gif", ".tif"];
-var fileList = [];
-
-$.ajax({url: dir,
-		success: function(data) {
-			var index = 0;
-			$(fileextension).each(function() {
-				$(data).find("a:contains(" + fileextension[index] + ")").each(function() {
-					if(fileList.indexOf(this.href.replace(window.location.host, "").replace("https://", "")) == -1) {
-						fileList.push(this.href.replace(window.location.host, "").replace("https://", ""));
-					}
-				});
-				index = index + 1;
-			});
-			fileList.sort();
-			
-			for(var i = 0; i < fileList.length; i++) {
-				var filename = fileList[i]
-            	var div = document.createElement("div");
-            	var rndNum = Math.floor((Math.random() * 6) + 1);
-            	if(rndNum == 1 || rndNum == 2 || rndNum == 3) {
-            		div.setAttribute("class", "sizeOne tile");
-            	} else if(rndNum == 4 || rndNum == 5) {
-            		div.setAttribute("class", "sizeTwo tile");
-            	} else if(rndNum == 6) {
-            		div.setAttribute("class", "sizeThree tile");
-            	}
-            	div.setAttribute("id", "image" + (i * 33));
-            	div.style.backgroundImage = 'url(' + dir + filename + ')';
-   	   		    div.content = "&nbsp";
-   	   		    div.onclick = function() {model(this.id)};
-    	        document.getElementById("flexBox").appendChild(div);
-			}
-		}
-});*/
-var dir = "/AbstractArt/images/";
-var fileList = ["a_red_11.jpg", "a_red_3.jpg", "e_cyan_4.jpg", "g_purple_1.jpg", "h_pink_7.png", "a_red_12.jpg", "b_orange_2.jpg", "f_blue_10.jpg", "g_purple_5.png", "a_red_13.jpg", "d_green_14.jpg", "f_blue_9.jpg", "h_pink_6.jpg"];
+var dir = "/home/anton/private/course/webbutveckling/AbstractArt/images/";
+var fileList = ["a_red_2.jpg", "a_red_3.jpg", "b_orange_6.jpg", "b_orange_8.jpg", "_9.jpg", "b_orange_10.jpg", "b_orange_11.jpg", "d_green_12.jpg", "g_purple_13.jpg", "b_orange_14.jpg", "h_pink_15.jpg", "h_pink_16.png", "f_blue_18.jpg", "b_orange_19.jpg", "f_blue_21.jpg", "b_orange_22.jpg", "a_red_23.jpg", "a_red_24.jpg", "f_blue_25.jpg", "a_red_26.jpg", "g_purple_27.jpg", "g_purple_29.jpg", "b_orange_30.jpg", "b_orange_31.jpg", "b_orange_32.jpg", "f_blue_33.jpg", "f_blue_35.jpg", "f_blue_37.jpg", "a_red_38.jpg", "f_blue_39.jpg", "a_red_40.jpg", "a_red_41.jpg", "f_blue_42.jpg", "f_blue_43.jpg", "b_orange_44.jpg", "a_red_45.jpg", "g_purple_46.png", "b_orange_47.jpg", "a_red_48.jpg", ""];
 fileList.sort();
 
 for(var i = 0; i < fileList.length; i++) {
-	var filename = fileList[i]
-    var div = document.createElement("div");
-    var rndNum = Math.floor((Math.random() * 6) + 1);
-    if(rndNum == 1 || rndNum == 2 || rndNum == 3) {
-    	div.setAttribute("class", "sizeOne tile");
-	} else if(rndNum == 4 || rndNum == 5) {
-    	div.setAttribute("class", "sizeTwo tile");
-    } else if(rndNum == 6) {
-    	div.setAttribute("class", "sizeThree tile");
+    if(fileList[i] !== "") {
+        var filename = fileList[i]
+        var div = document.createElement("div");
+        var rndNum = Math.floor((Math.random() * 6) + 1);
+        if(rndNum == 1 || rndNum == 2 || rndNum == 3) {
+            div.setAttribute("class", "sizeOne tile");
+        } else if(rndNum == 4 || rndNum == 5) {
+            div.setAttribute("class", "sizeTwo tile");
+        } else if(rndNum == 6) {
+            div.setAttribute("class", "sizeThree tile");
+        }
+        div.setAttribute("id", "image" + (i * 33));
+        div.style.backgroundImage = 'url(' + dir + filename + ')';
+        div.content = "&nbsp";
+        div.onclick = function() {model(this.id)};
+        document.getElementById("flexBox").appendChild(div);
     }
-    div.setAttribute("id", "image" + (i * 33));
-    div.style.backgroundImage = 'url(' + dir + filename + ')';
-   	div.content = "&nbsp";
-   	div.onclick = function() {model(this.id)};
-	document.getElementById("flexBox").appendChild(div);
 }
